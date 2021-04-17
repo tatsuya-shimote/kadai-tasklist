62e0d5a416e8fca947aad8864e73bf14689c6543
tatsuya.shimote <tanosholand@icloud.com>
2021-04-17 11:52:25 +0000

    add column

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+     validates :content, presence: true, length: {maximum:255}
+     #定義されていないバリデーションを先に作るとsave, updateメソッドでエラーが出る。
+     validates :status, presence: true, length: {maximum:10}
 end
