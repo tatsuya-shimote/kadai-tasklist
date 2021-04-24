f2cbbaccbf34828537f089482572e66539a1b854
tatsuya.shimote <tanosholand@icloud.com>
2021-04-24 12:58:31 +0000

    login feature improvement

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task , only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only:[:show, :edit, :destroy]
-  before_action :require_user_logged_in , only: [:index, :show, :new, :edit, :destroy]
+  before_action :correct_user, only:[:destroy]
+  
   
   
   def index
@@ -51,12 +52,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    if logged_in?
       @task = current_user.tasks.find_by(params[:id])
-    else
-      redirect_to root_url
-    end
-    
   end
 
   # Strong Parameter
