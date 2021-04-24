3479bb901d7e027d1fdaa6ba3e2fea2e141e0cc6
tatsuya.shimote <tanosholand@icloud.com>
2021-04-24 13:38:35 +0000

    login feature improvement

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,7 +52,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-      @task = current_user.tasks.find_by(params[:id])
+      @task = current_user.tasks.find(params[:id])
   end
 
   # Strong Parameter
